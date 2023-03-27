337bcb247be9befcca029918110ce1360111395a
andre <andrevzray@gmail.com>
2023-03-27 17:00:46 -0300

    back end fixed

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-require("express-async-errors");
-
 require('dotenv/config')
+
+require("express-async-errors");
 
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
